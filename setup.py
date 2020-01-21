501a8b52b397a980eb054b5f774578c5c4b4d424
Manos Kirtas <manolis.kirt@gmail.com>
2020-01-21 16:25:41 +0200

    Test Release 'v0.0.0.11-pre'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="deepbots",
     author="aidudezzz",
     author_email="deepbots@protonmail.com",
-    version="0.0.0.10-pre",
+    version="0.0.0.11-pre",
     description=
     "A wrapper framework for Reinforcement Learning in Webots simulator",
     long_description=readme,
