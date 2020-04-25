0634930306158220d98fa0fc2d2dd45868ab182b
Manos Kirtas <manolis.kirt@gmail.com>
2020-04-25 16:21:49 +0300

    Release 0.1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="deepbots",
     author="aidudezzz",
     author_email="deepbots@protonmail.com",
-    version="0.1.0",
+    version="0.1.0.1",
     description=DESCRIPTION,
     long_description=readme,
     long_description_content_type="text/markdown",
