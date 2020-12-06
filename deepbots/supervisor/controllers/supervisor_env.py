49860c79e6039106b65b89818b48c03b0b2972ab
Kostas Tsampazis <27914645+tsampazk@users.noreply.github.com>
2020-12-07 00:26:39 +0200

    Docstrings enhancements in RobotSupervisor and SupevisorEnv (#46)
    * 1. Fixed several minor errors in the docstrings
    2. Added more information in the docstrings
    3. Clarifications
    
    * Additional information and clarifications

--- deepbots/supervisor/controllers/supervisor_env.py
+++ deepbots/supervisor/controllers/supervisor_env.py
@@ -7,16 +7,24 @@ class SupervisorEnv(ABC):
     """
     This class represents the basic template which contains the necessary
     methods to train a reinforcement learning algorithm. The interface class
-    follows the gym interface which is standardized in much many reinforcement
+    follows the gym interface which is standardized in many reinforcement
     learning algorithms. The OpenAI gym environment can be described by the
     following figure:
 
-     +----------+             (action)            +---------------+
-     |          |-------------------------------->|               |
-     |   Agent  |                                 | SupervisorEnv |
-     |          |<--------------------------------|               |
-     +----------+      (observation, reward)      +---------------+
+    +----------+             (action)            +---------------+
+    |          |-------------------------------->|               |
+    |   Agent  |                                 | SupervisorEnv |
+    |          |<--------------------------------|               |
+    +----------+      (observation, reward)      +---------------+
 
+    This class is not intended for user usage, but to provide a common
+    interface for all provided supervisor classes and make them
+    compatible with reinforcement learning agents that work with
+    the gym interface. Moreover, a problem-agnostic reset method is
+    provided. Please use any of the children supervisor  classes to be
+    inherited by your own classes, such as the RobotSupervisor class.
+    Nevertheless, advanced users can inherit this class to create
+    their own supervisor classes if they wish.
     """
 
     def __init__(self):
@@ -26,9 +34,7 @@ class SupervisorEnv(ABC):
     def get_observations(self):
         """
         Return the observations of the robot. For example, metrics from
-        sensors, camera image etc.
-
-
+        sensors, a camera image, etc.
 
         :returns: An object of observations
         """
@@ -37,13 +43,14 @@ class SupervisorEnv(ABC):
     @abstractmethod
     def step(self, action):
         """
-        Each timestep, the agent chooses  an action, and the environment
-        returns the observation, the reward and the state of the problem (done
-        or not).
+        On each timestep, the agent chooses an action for the previous
+        observation, *state_t*, and the environment returns the next
+        observation, *state_t+1*, the reward and whether the episode
+        is done or not.
 
-        observation: The observation from the environment
-        reward: The amount of reward achieved by the previous action.
-        is_done: If the problem is solved
+        observation: The next observation from the environment
+        reward: The amount of reward awarded on this step
+        is_done: Whether the episode is done
         info: Diagnostic information mostly useful for debugging.
 
         :param action: The agent's action
@@ -54,17 +61,19 @@ class SupervisorEnv(ABC):
     @abstractmethod
     def get_reward(self, action):
         """
+        Calculates and returns the reward for this step.
+
         :param action: The agent's action
-        :return: the amount of reward achieved by the previous action.
+        :return: The amount of reward awarded on this step
         """
         pass
 
     @abstractmethod
     def is_done(self):
         """
-        Used to inform the agent that the problem solved.
+        Used to inform the agent that the problem is solved.
 
-        :return: bool, True if the problem have been solved
+        :return: bool, True if the episode is done
         """
         pass
 
@@ -72,14 +81,17 @@ class SupervisorEnv(ABC):
         """
         Used to reset the world to an initial state.
 
-        Default implementation of reset method, using Webots-provided methods.
+        Default, problem-agnostic, implementation of reset method,
+        using Webots-provided methods.
 
-        *Note that this works properly only with Webots versions >R2020b and must be
-        overridden with a custom reset method when using earlier versions. It is backwards compatible
-        due to the fact that the new reset method gets overridden by whatever the user has previously
-        implemented, so an old supervisor such as SupervisorCSV can be migrated easily to use this class.
+        *Note that this works properly only with Webots versions >R2020b
+        and must be overridden with a custom reset method when using
+        earlier versions. It is backwards compatible due to the fact
+        that the new reset method gets overridden by whatever the user
+        has previously implemented, so an old supervisor can be migrated
+        easily to use this class.
 
-        :return: default observation provided by get_default_observation() implementation
+        :return: default observation provided by get_default_observation()
         """
         self.supervisor.simulationReset()
         self.supervisor.simulationResetPhysics()
@@ -87,8 +99,9 @@ class SupervisorEnv(ABC):
 
     def get_default_observation(self):
         """
-        This method should be implemented to return a default/starting observation
-        that is use-case dependant. It is mainly used by the reset implementation above.
+        This method should be implemented to return a default/starting
+        observation that is use-case dependant. It is used by the
+        reset implementation above.
 
         :return: list-like, contains default agent observation
         """
@@ -97,6 +110,7 @@ class SupervisorEnv(ABC):
     @abstractmethod
     def get_info(self):
         """
-         Diagnostic information mostly useful for debugging.
+        This method can be implemented to return any diagnostic
+        information on each step, e.g. for debugging purposes.
         """
         pass
