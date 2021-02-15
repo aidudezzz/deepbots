888a6eb2af19eb60bec61daeccb9ebabc1e92381
Kostas Tsampazis <27914645+tsampazk@users.noreply.github.com>
2021-02-15 20:11:09 +0200

    Some fixes in docstrings, removed ABC abstract and timestep->time_step for consistency with supervisor classes

--- deepbots/robots/controllers/robot_emitter_receiver.py
+++ deepbots/robots/controllers/robot_emitter_receiver.py
@@ -1,24 +1,23 @@
-from abc import ABC, abstractmethod
-
 from controller import Robot
 
 
-class RobotEmitterReceiver(ABC):
+class RobotEmitterReceiver:
     """
     This RobotEmitterReceiver implements only the most basic run method, that
-    steps the robot and calls the handleEmitter, handleReceiver methods that
+    steps the robot and calls the handle_emitter, handle_receiver methods that
     are needed for communication with the supervisor.
 
     This class must be inherited by all robot controllers created by the user
-    and the handleEmitter, handleReceiver, initialize_comms methods are all
-    abstract and need to be implemented, according to their docstrings. For a
+    and the handle_emitter, handle_receiver, initialize_comms methods are all
+    abstract and need to be implemented according to their docstrings. For a
     simpler RobotController that implements the methods in a basic form
-    inherit the RobotEmitterReceiver class.
+    inherit the RobotEmitterReceiverCSV subclass or other emitter-receiver
+    subclasses.
     """
     def __init__(self,
                  emitter_name="emitter",
                  receiver_name="receiver",
-                 timestep=None):
+                 time_step=None):
         """
         The basic robot constructor.
 
@@ -28,30 +27,32 @@ class RobotEmitterReceiver(ABC):
         Also initializes the emitter and the receiver used to communicate with
         the supervisor, using the initialize_comms() method which must be
         implemented by the user. The two methods handle_emitter() and
-        handle_receiver() must also be implemented by the user.
+        handle_receiver() are also implemented by the user.
 
         For the step argument see relevant Webots documentation:
         https://cyberbotics.com/doc/guide/controller-programming#the-step-and-wb_robot_step-functions
 
-        :param timestep: float, positive or None
+        :param time_step: int, positive or None
         """
         self.robot = Robot()
 
-        if timestep is None:
+        if time_step is None:
             self.timestep = int(self.robot.getBasicTimeStep())
         else:
-            self.timestep = timestep
+            self.timestep = time_step
 
         self.emitter, self.receiver = self.initialize_comms(
             emitter_name, receiver_name)
 
     def get_timestep(self):
+        # TODO maybe remove this altogether and make self.timestep
+        #  a pythonic class property. Print deprecation warning for
+        #  next version?
         return self.timestep
 
-    @abstractmethod
     def initialize_comms(self, emitter_name, receiver_name):
         """
-        This method should initialize and the return emitter and receiver in a
+        This method should initialize and return the emitter and receiver in a
         tuple as expected by the constructor.
 
         A basic example implementation can be:
@@ -63,18 +64,16 @@ class RobotEmitterReceiver(ABC):
 
         :return: (emitter, receiver) tuple, as initialized
         """
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
     def handle_emitter(self):
         """
         This method should take data from the robot, eg. sensor data, parse
         it into a message and use the robot's emitter to send it to the
         supervisor. This message will be used as the observation of the robot.
         """
-        pass
+        raise NotImplementedError
 
-    @abstractmethod
     def handle_receiver(self):
         """
         This method should take data through the receiver in the form of a
@@ -83,7 +82,7 @@ class RobotEmitterReceiver(ABC):
         For example the message might include a motor speed, which should be
         parsed and applied to the robot's motor.
         """
-        pass
+        raise NotImplementedError
 
     def run(self):
         """
