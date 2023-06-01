12be41448a88d416e46387e5a47c912f4e1a6e1d
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-06-01 17:16:43 +0300

    added autodock mock import controller

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,8 @@ epub_show_urls = "footnote"
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+autodoc_mock_imports = ["controller"]
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
