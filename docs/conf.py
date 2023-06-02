b878443edbeb4f66acc17f911958e9f8e87bbaf4
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-06-03 00:00:18 +0300

    Path insertion shenanigans

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../deepbots'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
