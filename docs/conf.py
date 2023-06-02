0eea79b848b8005a2cc017122910ecb17a106a22
tsampazk <27914645+tsampazk@users.noreply.github.com>
2023-06-02 15:26:56 +0300

    Trying out logo

--- docs/conf.py
+++ docs/conf.py
@@ -64,3 +64,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_logo = 'https://raw.githubusercontent.com/aidudezzz/deepbots-swag/main/logo/deepbots.png'
+# html_favicon = 'favicon.ico'
